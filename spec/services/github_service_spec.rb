5108a9c0ae96543a91b1f0cd10cead30c1371c16
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-07 13:25:40 -0700

    Add tabs to profile view Add vcr to tests

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "GithubService" do
   context "#repos_by_user" do
-    it "returns a user's repos" do
+    it "returns a user's repos", :vcr do
       user_repos = GithubService.new.repos_by_user("epintozzi")
 
       expect(user_repos).to have_key(:name)
@@ -10,7 +10,7 @@ describe "GithubService" do
   end
 
   context "#all_commits_by_user" do
-    it "returns a user's commits" do
+    it "returns a user's commits", :vcr do
       user_commits = GithubService.new.all_commits_by_user("epintozzi")
 
       push_events = user_commits.first.has_value?("PushEvent")
@@ -20,7 +20,7 @@ describe "GithubService" do
   end
 
   context "#commits_by_user_repo" do
-    it "returns a user's commits by repo" do
+    it "returns a user's commits by repo", :vcr do
       user_commits_by_repo = GithubService.new.commits_by_user_repo("epintozzi")
 
       expect(user_commits_by_repo).to have_key(:sha)
@@ -28,7 +28,7 @@ describe "GithubService" do
   end
 
   context "#commit_details_by_repo_url" do
-    it "returns a user's commit by repo url" do
+    it "returns a user's commit by repo url", :vcr do
       user_commits_by_repo_url = GithubService.new.commit_details_by_repo_url("https://api.github.com/repos/epintozzi/rales_engine/commits/85c1562f955b843c26b9e4210064a7baede9c1fa")
 
       message = user_commits_by_repo_url[:commit][:message]
@@ -38,7 +38,7 @@ describe "GithubService" do
   end
 
   context "#user_repos" do
-    it "returns all repositories for a user" do
+    it "returns all repositories for a user", :vcr do
       user_repos = GithubService.new.user_repos("epintozzi")
       repo = user_repos.first
 
