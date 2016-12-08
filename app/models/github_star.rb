19a5873a1ca7a64b9057f58fd5fe9d75c4c2196f
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-07 20:48:52 -0700

    Add github_star model and tests

---
+++ app/models/github_star.rb
@@ -0,0 +1,22 @@
+class GithubStar
+  attr_reader :name, :full_name, :description, :language, :updated_at
+
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @language = attributes[:language]
+    @description = attributes[:description]
+    @updated_at = attributes[:updated_at]
+  end
+
+  def self.for_user(user)
+    GithubService.new.starred(user).map do |raw_repo|
+      GithubStar.new(raw_repo)
+    end
+  end
+
+  def date_format(date_string)
+    date = date_string.to_datetime
+    date.strftime("%b %e")
+  end
+
+end
---
+++ spec/models/github_star_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe GithubStar do
+  context ".for_user(user)" do
+    it "returns repos for a user", :vcr do
+        user = User.new(token: ENV["github_user_token"])
+        starred_repos = GithubStar.for_user(user)
+        repo = starred_repos.first
+
+        expect(starred_repos).to be_an(Array)
+        expect(repo).to be_a(GithubStar)
+        expect(repo).to respond_to(:name)
+        expect(repo).to respond_to(:language)
+        expect(repo).to respond_to(:description)
+        expect(repo).to respond_to(:updated_at)
+    end
+  end
+
+  context "date_format(date_string)" do
+    it "returns formatted date", :vcr do
+        user = User.new(token: ENV["github_user_token"])
+        starred_repos = GithubStar.for_user(user)
+        repo = starred_repos.first
+        date = repo.date_format(repo.updated_at)
+
+        expect(starred_repos).to be_an(Array)
+        expect(repo).to be_a(GithubStar)
+    end
+  end
+
+end
