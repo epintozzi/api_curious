e2ab528127e5bd864243b8321f7ed00689fa05c3
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-08 11:15:13 -0700

    Feature testing repos, stars, following, followers

--- spec/features/repos/user_sees_list_of_their_repos_spec.rb
+++ spec/features/repos/user_sees_list_of_their_repos_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe "a user can see list of followers" do
-  scenario "user sees list of their followers", :vcr do
+describe "a user can see list of their repos" do
+  scenario "user sees list of their repos", :vcr do
       user = create(:user, username: "epintozzi")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit followers_path
+      visit repos_path
 
-      expect(page).to have_content "pyro2927"
+      expect(page).to have_content "api_curious"
   end
 end
